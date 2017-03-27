25117aba22056184fa040e44dd7d66b89abbc20a
Analia Mok <axm2580@g.rit.edu>
2017-03-26 20:50:43 -0400

    Added color transition to icon hovers

---
+++ assets/js/personal_web-1.0.0.js
@@ -0,0 +1,16 @@
+/**
+ * File Name: personal_web-1.0.0.js
+ * Description: Main javascript file for personal website. 
+ * Used by index.html
+ *
+ * @author: Analia Mok
+ */
+
+/**
+ * openLink - Opens appropriate link in another 
+ * browser tab
+ * @param link to website
+ */
+function openLink(webLink) {
+    window.open(webLink);
+} // End of openLink
